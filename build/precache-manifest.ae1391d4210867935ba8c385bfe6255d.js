a16d54597e152ed3cfdfaa9e1dd4e897bd33385c
muzammil-15 <121623043+muzammil-15@users.noreply.github.com>
2023-02-06 21:12:17 +0500

    Add files via upload

---
+++ build/precache-manifest.ae1391d4210867935ba8c385bfe6255d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "33c6a457774cf7ed1bf6",
+    "url": "/static/css/main.8417af22.chunk.css"
+  },
+  {
+    "revision": "33c6a457774cf7ed1bf6",
+    "url": "/static/js/main.33c6a457.chunk.js"
+  },
+  {
+    "revision": "ff3e70574e23ec862a62",
+    "url": "/static/js/1.ff3e7057.chunk.js"
+  },
+  {
+    "revision": "7f217853f64424ac3e35",
+    "url": "/static/css/2.15630c5c.chunk.css"
+  },
+  {
+    "revision": "7f217853f64424ac3e35",
+    "url": "/static/js/2.7f217853.chunk.js"
+  },
+  {
+    "revision": "452ecbd6c2de88b254e0",
+    "url": "/static/js/runtime~main.452ecbd6.js"
+  },
+  {
+    "revision": "2c5b074ef6fa78394a0bc234b7af0542",
+    "url": "/static/media/Image2.2c5b074e.png"
+  },
+  {
+    "revision": "26fc186b76a7fbd46939cffd928c921f",
+    "url": "/static/media/cal.26fc186b.png"
+  },
+  {
+    "revision": "f63acab7aba8432c43fc3d7bdabc8243",
+    "url": "/static/media/glass.f63acab7.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "fc0327ae15d84b545588f53ad5d10366",
+    "url": "/index.html"
+  }
+];
